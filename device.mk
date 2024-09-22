7f06a6f340badc35b19aeca771071f038a2fe474
Sanjivns <alshahab109@gmail.com>
2024-09-22 05:19:21 +0000

    spes: remove vendor.qti.hardware.vibrator.service
    Build Error:
    FAILED: //vendor/qcom/opensource/vibrator/aidl:vendor.qti.hardware.vibrator.impl generate toc vendor.qti.hardware.vibrator.impl.so
    Outputs: out/soong/.intermediates/vendor/qcom/opensource/vibrator/aidl/vendor.qti.hardware.vibrator.impl/android_vendor.33_arm64_armv8-a_shared/vendor.qti.hardware.vibrator.impl.so.toc
    Error: exited with code: 1
    Command: echo "module vendor.qti.hardware.vibrator.impl missing dependencies: qti_vibrator_hal_defaults" && false
    Output:
    module vendor.qti.hardware.vibrator.impl missing dependencies: qti_vibrator_hal_defaults
    
    Signed-off-by: Sanjivns <alshahab109@gmail.com>

--- device.mk
+++ device.mk
@@ -489,7 +489,6 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
