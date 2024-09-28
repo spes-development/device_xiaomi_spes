f265d060de288418fb3ab5908307b6c8cc990ac8
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-09-28 17:26:35 +0000

    spes: update branches and sources
    Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 echo "Syncing additional trees"
 rm -rf hardware/xiaomi/megvii
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-18.1
-git clone https://github.com/halt-spesn/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/spes --depth=1
+git clone https://github.com/spes-development/hardware_xiaomi -b lineage-18.1
+git clone https://github.com/spes-development/vendor_xiaomi_spes -b 11.0 vendor/xiaomi/spes
+git clone https://github.com/spes-development/kernel_xiaomi_sm6225 -b NaughtySilver kernel/xiaomi/sm6225 --depth=1
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-git clone https://github.com/halt-spesn/vendor_xiaomi_miui_camera vendor/xiaomi/camera --depth=1
+git clone https://github.com/spes-development/vendor_xiaomi_miui_camera vendor/xiaomi/camera --depth=1
