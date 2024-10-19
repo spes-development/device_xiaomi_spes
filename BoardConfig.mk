c7a6e35644bdc9c74d3cab8e089b4bff33304c10
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2024-10-19 15:21:22 +0300

    spes: props: Move props to configs folder
    For better organization and easier management:
    - odm.prop -> configs/props/odm.prop
    - product.prop -> configs/props/product.prop
    - system.prop -> configs/props/system.prop
    - system_ext.prop -> configs/props/system_ext.prop
    - vendor.prop -> configs/props/vendor.prop
    
    Change-Id: I72606380750d7403e0564842a7d9d8bd4eeb51c5
    Signed-off-by: tanvirr007 <tanvir.mymnsngh@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -221,11 +221,11 @@ TARGET_BOARD_PLATFORM := bengal
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
