d2e906d91a88b1c989b1577259576e3b8263ad01
Royna2544 <whiteshell2544@naver.com>
2024-11-07 13:32:28 +0600

    spes: Include Debugging-Tools
    After booting, use a file manager to navigate to the /data/debug/boot directory, where you will find all logs, including kernel dmesg logs
    
    If bootloop:
    1. Reboot into recovery
    2. Use file manager or ADB to access /data/debug/boot directory
    3. Collect the log files in this directory and share them with the maintainer for debugging
    
    Signed-off-by: tanvirr007 <tanvirr007@github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -283,3 +283,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/xiaomi/spes/BoardConfigVendor.mk
+
+# Include debug tool
+include hardware/samsung-ext/interfaces/sepolicy/SEPolicy.mk
