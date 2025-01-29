f856699208deed5a4e031cfc8a6dea68c8992f7c
tanvirr007 <md.tanvirhasannijum@gmail.com>
2025-01-29 14:03:03 +0600

    spes: Cleanup Device Tree
    - This is a combination of 8 commits.
    - This is the 1st commit message:
    
    Revert "spes: overlay: Add updater support"
    
    This reverts commit 40c5945b8c461240a72a2ac46efaa1af31f6412a.
    
    Revert "spes: Moving to own fork of hardware repo"
    
    This reverts commit ff72ec3bab29fe05290c12e39dcc0c3b9259fb83.
    
    Revert "spes: Maintainer name for @custom-crdroid by @drkphnx"
    
    This reverts commit c11be1cb47232efe239378facc539362c48512d2.
    
    Revert "spes: Shipping BCR in build"
    
    This reverts commit b6a0d1c4f1f981915569337bc052d17aa0d04c3f.
    
    Revert "spes: Disable shipping Seedvault in build"
    
    This reverts commit 01c838b2e8ce833eb606c67f3596ad16b1fe282f.
    
    Revert "spes: overlay: Disable turbulence noise"
    
    This reverts commit 4565a86194f799aaf7c78b5d0874177151c7d448.
    
    Revert "spes: overlay: Disable HEVC toggle in Screen record dialog"
    
    This reverts commit 164f41f9ee6d9fa01d5de5b7aa5241f9bb706a4d.
    
    Revert "spes: Init @Evolution-X"
    
    This reverts commit 28cfb384940ba770394b3b5593f211e7a7228073.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/euclid_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    euclid_spes-user \
+    euclid_spes-userdebug \
+    euclid_spes-eng
